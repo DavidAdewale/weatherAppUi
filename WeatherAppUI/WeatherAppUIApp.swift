3ae5b00c4e5f70211dc44cdd51e496631b9f7d86
DavidAdewale <adewaledavidola@gmail.com>
2024-07-10 17:51:15 +0100

    Initial Commit

---
+++ WeatherAppUI/WeatherAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppUIApp.swift
+//  WeatherAppUI
+//
+//  Created by David Adewale on 10/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
